1096847abe778da52767bc5a5d08f85fd0d4ec7d
BudgetToaster <28935064+BanePig@users.noreply.github.com>
2021-02-28 19:49:21 -0500

    Fixed ToBarycentricCoordinates in OctreeMath
    Previously did not project point onto triangle, so barycentric coordinates did not work correctly.

--- SVO/Utils/OctreeMath.cs
+++ SVO/Utils/OctreeMath.cs
@@ -7,10 +7,14 @@ namespace SVO
     {
         public static Vector3 ToBarycentricCoordinates(Vector3 point, Vector3 p1, Vector3 p2, Vector3 p3)
         {
-            var a1 = CalculateTriangleArea(point, p2, p3);
-            var a2 = CalculateTriangleArea(p1, point, p3);
-            var a3 = CalculateTriangleArea(p1, p2, point);
-            return new Vector3(a1, a2, a3) / CalculateTriangleArea(p1, p2, p3);
+            var norm = Vector3.Cross(p2 - p1, p3 - p1);
+            var pointProj = point - (Vector3.Dot(point - p1, norm) / norm.sqrMagnitude * norm);
+            
+            
+            var a1 = CalculateTriangleArea(pointProj, p2, p3);
+            var a2 = CalculateTriangleArea(p1, pointProj, p3);
+            var a3 = CalculateTriangleArea(p1, p2, pointProj);
+            return new Vector3(a1, a2, a3) / (a1 + a2 + a3);
         }
 
         public static float CalculateTriangleArea(Vector3 p1, Vector3 p2, Vector3 p3)
